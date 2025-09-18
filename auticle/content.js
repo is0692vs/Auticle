1b255d1c9fcc40affa82ecb6d7579a9434404915
is0692vs <is0692vs@ed.ritsumei.ac.jp>
2025-09-18 13:48:37 +0900

    feat: メッセージリスナーの追加を条件付きで実行するように変更

--- auticle/content.js
+++ auticle/content.js
@@ -1,7 +1,14 @@
 console.log("Content script placeholder.");
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.command === "stateChange") {
-    console.log("Auticle state changed to:", message.enabled);
-  }
-});
+if (!window.auticleListenerAdded) {
+  chrome.runtime.onMessage.addListener(function (
+    message,
+    sender,
+    sendResponse
+  ) {
+    if (message.command === "stateChange") {
+      console.log("Auticle state changed to:", message.enabled);
+    }
+  });
+  window.auticleListenerAdded = true;
+}
